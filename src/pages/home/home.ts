61c45b609b1f4793b94a3c7638b43182a5174c56
Sgajjar08 <saurabhgajjar134@gmail.com>
2018-07-31 16:05:00 +0300

    finish

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as $ from "jquery";
+import 'slick-carousel/slick/slick';
 
 @Component({
   selector: 'page-home',
@@ -11,4 +13,13 @@ export class HomePage {
 
   }
 
+  ngOnInit() {
+    $('.myCarousel').slick({
+      dots: true,
+      centerMode: true,
+      infinite: false,
+      centerPadding: '60px',
+      slidesToShow: 1
+    });
+}
 }
